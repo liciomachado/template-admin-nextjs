2e0ae4fcf643dc46fe872d2e11d4e15e04facc93
Mauricio <licio.machado@hotmail.com>
2021-08-26 17:04:53 -0300

    first commit

---
+++ src/components/auth/AuthInput.tsx
@@ -0,0 +1,25 @@
+
+interface AuthInputProps {
+    label: string
+    valor: any
+    obrigatorio?: boolean
+    naoRenderizarQuando?: boolean
+    tipo?: 'text' | 'email' | 'password'
+    valorMudou: (novoValor: any) => void
+}
+
+export default function AuthInput(props: AuthInputProps) {
+    return props.naoRenderizarQuando ? null : (
+        <div className="flex flex-col mt-4">
+            <label htmlFor="">{props.label}</label>
+            <input type={props.tipo ?? 'text'}
+                value={props.valor}
+                required={props.obrigatorio}
+                className={`px-4 py-3 rounded-lg bg-gray-200 mt-2
+                    border focus:border-blue-500 focus:bg-white
+                    focus:outline-none
+                `}
+                onChange={e => props.valorMudou?.(e.target.value)} />
+        </div>
+    )
+}
---
+++ src/pages/autenticacao.tsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import AuthInput from "../components/auth/AuthInput";
+import { IconeAtencao } from "../components/icons";
+import useAuth from "../data/hook/useAuth";
+
+export default function Autenticacao() {
+
+    const { usuario, loginGoogle } = useAuth()
+
+    const [erro, setErro] = useState(null)
+    const [modo, setModo] = useState<'login' | 'cadastro'>('login')
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    function exibirErro(msg, tempoSegundos = 5) {
+        setErro(msg)
+        setTimeout(() => setErro(null), tempoSegundos * 1000)
+    }
+
+    function submeter() {
+        if (modo === 'login') {
+            console.log('Login')
+            exibirErro('Ocorreu um erro no login!')
+        } else {
+            console.log('cadastrar')
+            exibirErro('Ocorreu um erro no cadastro!')
+
+        }
+
+    }
+
+    //https://source.unsplash.com/collection/2799326
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <div className="hidden md:block md:w-1/2 lg:w-2/3">
+                <img src="https://source.unsplash.com/featured/?military"
+                    alt="imagem tela" className="h-screen w-full object-cover" />
+            </div>
+            <div className="m-10 w-full md:w-1/2 lg:w-1/3">
+                <h1 className={`text-3xl font-bold mb-5`}>
+                    {modo === 'login' ? 'Entre com a sua conta' : 'Cadastre-se na Plataforma'}
+                </h1>
+                {erro ? (
+                    <div className="flex items-center bg-red-400 text-white py-3 px-5 my-2 
+                        border border-red-700 rounded-lg">
+                        {IconeAtencao()}
+                        <span className="ml-3">{erro}</span>
+                    </div>
+                ) : false
+                }
+
+
+                <AuthInput label="Email"
+                    valor={email}
+                    tipo="email"
+                    valorMudou={setEmail}
+                    obrigatorio
+                />
+
+                <AuthInput label="Senha"
+                    valor={senha}
+                    tipo="password"
+                    valorMudou={setSenha}
+                    obrigatorio
+                />
+
+                <button onClick={submeter} className={` w-full bg-indigo-500 hover:bg-indigo-400
+                text-white rounded-lg px-4 py-3 mt-6
+            `}>
+                    {modo === 'login' ? 'Entrar' : 'Cadastrar'}
+                </button>
+
+                <hr className="my-6 border-gray-300 w-full" />
+
+                <button onClick={loginGoogle} className={` w-full bg-red-500 hover:bg-red-400
+                text-white rounded-lg px-4 py-3
+            `}>
+                    Entrar com Google
+                </button>
+
+                {modo === 'login' ? (
+                    <p className="mt-8">
+                        Novo por aqui?
+                        <a onClick={() => setModo('cadastro')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold cursor-pointer
+                        `}> Crie uma conta gratuitamente</a>
+                    </p>
+                ) : (
+                    <p className="mt-8">
+                        Já faz parte da nossa comunidade?
+                        <a onClick={() => setModo('login')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold cursor-pointer
+                        `}> Entre com as suas credenciais</a>
+                    </p>
+                )}
+            </div>
+        </div>
+    )
+}
